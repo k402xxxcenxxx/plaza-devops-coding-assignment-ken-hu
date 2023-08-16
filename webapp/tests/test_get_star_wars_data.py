564fa0f0e6508aaa76311574a0b8f9257eb571f3
k402xxxcenxxx <k402xxxcenxxx@gmail.com>
2023-08-17 01:51:59 +0800

    Add: test for bmi api

--- webapp/tests/test_get_star_wars_data.py
+++ webapp/tests/test_get_star_wars_data.py
@@ -15,3 +15,12 @@ def test_default_hello():
     assert response.status_code == 200
     assert "message" in response.json()
     assert "Hello, World!" == response.json()['message']
+
+def test_get_top_people_by_bmi():
+    client = TestClient(app)
+    response = client.get("/top-people-by-bmi")
+    assert response.status_code == 200
+    assert 20 == len(response.json())
+    assert "name" in response.json()[0]
+    assert "bmi" in response.json()[0]
+    assert "Dud Bolt" == response.json()[0]['name']
