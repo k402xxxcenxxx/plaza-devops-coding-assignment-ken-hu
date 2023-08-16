8b3ed17454511b3be904b53d2d1e5def1e6e6476
k402xxxcenxxx <k402xxxcenxxx@gmail.com>
2023-08-17 01:47:51 +0800

    Add: hello test Fix: get data api test with given parameter

--- webapp/tests/test_get_star_wars_data.py
+++ webapp/tests/test_get_star_wars_data.py
@@ -4,8 +4,14 @@ from webapp.src.app import app
 
 def test_get_star_wars_data():
     client = TestClient(app)
-    response = client.get("/data")
+    response = client.get("/data/1")
     assert response.status_code == 200
     assert "name" in response.json()
 
 # Additional test cases can be added by the candidate
+def test_default_hello():
+    client = TestClient(app)
+    response = client.get("/")
+    assert response.status_code == 200
+    assert "message" in response.json()
+    assert "Hello, World!" == response.json()['message']
